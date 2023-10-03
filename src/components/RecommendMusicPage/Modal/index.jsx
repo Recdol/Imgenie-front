9e9f7ea0682f5cd84e133ec4f3a74cd1ca29ffd4
2jun0 <soo28819@gmail.com>
2023-10-04 00:10:09 +0900

    feat: update exit button at modal page

--- src/components/RecommendMusicPage/Modal/index.jsx
+++ src/components/RecommendMusicPage/Modal/index.jsx
@@ -2,29 +2,21 @@ import { React } from 'react';
 import './style.css';
 import PropTypes from 'prop-types';
 import CardGen from './CardGenerator';
+import exitSvg from './svgs/exit.svg';
 
 function Modal({ setOpenModal, imgUrl, artistName, musicTitle }) {
   return (
     <div className="modalBackground">
       <div className="modalContainer">
-        <div className="titleCloseBtn">
-          <button
-            type="button"
-            onClick={() => {
-              setOpenModal(false);
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="20"
-              viewBox="0 -960 960 960"
-              width="20"
-              fill="#8B8B8C"
-            >
-              <path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z" />
-            </svg>
-          </button>
-        </div>
+        <button
+          className="titleCloseBtn"
+          type="button"
+          onClick={() => {
+            setOpenModal(false);
+          }}
+        >
+          <img src={exitSvg} alt="exit icon" />
+        </button>
         <CardGen
           imgUrl={imgUrl}
           musicTitle={musicTitle}
